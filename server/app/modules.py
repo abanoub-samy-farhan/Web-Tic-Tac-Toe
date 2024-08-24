de8ef60a57bcc593b1624123756e86ba11e519cd
Emran <ouakouriimran@gmail.com>
2024-08-24 19:44:23 +0100

    adding columns

--- server/app/modules.py
+++ server/app/modules.py
@@ -1,22 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Integer, Column
 from app import app
 from app import db
 
+ 
+
+class GameStatusEnum(db.Enum):
+    WIN = "win"
+    LOSE = "lose"
+    DRAW = "draw"
+
+   
 class User(db.Model):
     user_id = db.Column(db.String(240), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    upadted_at = db.Column(db.DateTime, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     hashed_password = db.Column(db.String(240), nullable=False)
+    avatar = db.Column(db.String(240), nullable=False)
+    about = db.Column(db.String(240), nullable=False)
+    banner = db.Column(db.String(240), nullable=False)
     def __repr__(self):
         return '<User %r>' % self.username
     
 class Game(db.Model):
     game_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(240), db.ForeignKey('user.user_id'), nullable=False)
-    game_winner = db.Column(db.String(80), nullable=True)
-    game_lose = db.Column(db.String(80), nullable=True)
-    game_draw = db.Column(db.String(80), nullable=True)
+    oppenent_id = db.Column(db.String(240), db.ForeignKey('user.user_id'), nullable=False)
+    game_status = db.Column(db.Enum(GameStatusEnum), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
     def __repr__(self):
         return '<Game %r>' % self.game_id
