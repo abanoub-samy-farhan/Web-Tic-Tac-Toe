10a95c347c9454245ee2508228229d60f6e400ba
Salma Fithi <128839637+Semagrb@users.noreply.github.com>
2024-08-25 22:15:11 -0400

    Update and rename whats-this.html to whats-this.js

---
+++ front-end/whats-this.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function WhatsThis() {
+    return (
+        <section className="content">
+            <h1>What’s This?</h1>
+            <p>Welcome to the Web Tic Tac Toe showdown! This is not just any Tic Tac Toe game. This is where legends are born, friendships are tested, and X's and O's find their true purpose.</p>
+            <p>Think you’ve mastered Tic Tac Toe? Think again! Here, you can challenge your friends, battle against the computer, and even take your game online for the ultimate competition.</p>
+            <p>We’ve added some extra sprinkles of fun with real-time multiplayer action and a dash of friendly banter. So, grab your thinking cap (or your lucky socks) and dive into the ultimate Tic Tac Toe experience!</p>
+            <p><a href="/get-started" className="btn">Ready to Play? Let’s Go!</a></p>
+        </section>
+    );
+}
+
+export default WhatsThis;
