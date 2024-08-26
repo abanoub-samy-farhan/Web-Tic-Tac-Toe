c780a802a32601050409b24071e11835f92052cc
Salma Fithi <128839637+Semagrb@users.noreply.github.com>
2024-08-25 22:14:00 -0400

    Update and rename terms-of-service.html to terms-of-service.js

---
+++ front-end/terms-of-service.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function TermsOfService() {
+    return (
+        <section className="content">
+            <h1>Terms of Service</h1>
+            <p>Welcome to Web Tic Tac Toe! By using our game, you agree to the following:</p>
+            <ul>
+                <li>You play at your own risk, especially when facing off against the Tic Tac Toe Master (a.k.a. the computer).</li>
+                <li>All your victories and defeats are stored in our database—forever. Choose your moves wisely!</li>
+                <li>No cheating! We’ve got our eyes on you, and any funny business will result in an immediate timeout.</li>
+                <li>Our Tic Tac Toe game is for fun, and we aim to keep it that way. Please be kind and respectful to other players.</li>
+            </ul>
+            <p>Remember, it's all about having fun, sharpening your strategy skills, and maybe earning a few bragging rights along the way.</p>
+            <p>Let’s keep it friendly, fair, and full of fun!</p>
+        </section>
+    );
+}
+
+export default TermsOfService;
